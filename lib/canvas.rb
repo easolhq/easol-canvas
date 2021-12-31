97aec4b201ca26522ee04fecd92fa89ae58d8926
Kyle Byrne <kyletbyrne96@gmail.com>
2021-12-31 12:22:40 +0000

    Move checks into check subfolder

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -3,8 +3,9 @@ require_relative "canvas/cli"
 require_relative "canvas/lint"
 require_relative "canvas/validators/html"
 require_relative "canvas/check"
-require_relative "canvas/template_check"
 require_relative "canvas/offense"
+
+Dir[__dir__ + "/canvas/checks/*.rb"].each { |file| require file }
 
 module Canvas
 end
--- lib/canvas/template_check.rb
+++
@@ -1,8 +0,0 @@
-module Canvas
-  class TemplateCheck < Check
-    self.base_folder = "templates"
-
-    require_file "product/index.liquid"
-    validate_html "product/index.liquid"
-  end
-end
