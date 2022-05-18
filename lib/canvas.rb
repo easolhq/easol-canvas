f06574648e5a0f497facb962dacc663fb53e8832
Ian Mooney <iancmooney@gmail.com>
2022-05-18 18:21:24 +0100

    Add ValidLiquidCheck

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -2,6 +2,7 @@ require_relative "canvas/version"
 require_relative "canvas/cli"
 require_relative "canvas/lint"
 require_relative "canvas/validators/html"
+require_relative "canvas/validators/liquid"
 require_relative "canvas/check"
 require_relative "canvas/offense"
 require_relative "canvas/checks"
@@ -10,6 +11,7 @@ Dir[__dir__ + "/canvas/{checks,services}/*.rb"].each { |file| require file }
 
 Canvas::Checks.register(Canvas::RequiredFilesCheck)
 Canvas::Checks.register(Canvas::ValidHtmlCheck)
+Canvas::Checks.register(Canvas::ValidLiquidCheck)
 
 module Canvas
 end
---
+++ spec/canvas/checks/valid_liquid_check_spec.rb
@@ -0,0 +1,28 @@
+describe Canvas::ValidLiquidCheck do
+  include ExampleDirectoryHelper
+
+  subject(:check) { Canvas::ValidLiquidCheck.new }
+
+  describe "#run" do
+    it "doesn't add any errors if all files contain valid liquid" do
+      copy_example_directory("vagabond")
+      subject.run
+      expect(subject.offenses).to be_empty
+    end
+
+    it "adds an offense when a file contains invalid liquid" do
+      copy_example_directory("alchemist")
+      subject.run
+      message = <<~MESSAGE.chop
+        Invalid Liquid: templates/blog_post/index.liquid - \n
+        Liquid syntax error (line 2): 'if' tag was never closed
+      MESSAGE
+
+      expect(subject.offenses).to match_array(
+        [
+          have_attributes(message: message.squeeze("\n"))
+        ]
+      )
+    end
+  end
+end
