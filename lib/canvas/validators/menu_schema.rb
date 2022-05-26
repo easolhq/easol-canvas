f606212d618557a8f567ea5f2ed388aba2981e28
Ian Mooney <iancmooney@gmail.com>
2022-05-26 12:09:05 +0100

    Add MenuSchema validator
    This validator can be used to validate the schema used for a menu.

---
+++ lib/canvas/validators/menu_schema.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+module Canvas
+  module Validator
+    # :documented:
+    # This class is used to validate a schema for a menu.
+    #
+    # Example:
+    # {
+    #   "max_item_levels": 2,
+    #   "supports_open_new_tab": "true",
+    #   "attributes": {
+    #      "fixed": {
+    #         "group": "design",
+    #         "label": "Fixed when scrolling",
+    #         "hint": "The menu will stay fixed to the top when scrolling down the page.",
+    #         "type": "boolean",
+    #         "default: "false"
+    #      }
+    #   }
+    # }
+    #
+    class MenuSchema
+      PERMITTED_KEYS = %w[max_item_levels supports_open_new_tab attributes].freeze
+      ADDITIONAL_RESERVED_NAMES = %w[items type].freeze
+
+      attr_reader :schema, :errors
+
+      # @param schema [Hash] the schema to be validated
+      # @param custom_types [Array<Hash>] a list of custom types
+      def initialize(schema:, custom_types: [])
+        @schema = schema
+        @custom_types = custom_types
+        @errors = []
+      end
+
+      def validate
+        if ensure_valid_format
+          ensure_no_unrecognized_keys
+          ensure_max_item_levels_is_valid
+          ensure_attributes_are_valid
+        end
+
+        @errors.empty?
+      end
+
+      private
+
+      def ensure_valid_format
+        return true if schema.is_a?(Hash) &&
+                       (schema["attributes"].nil? || attributes_array_of_hashes?(schema))
+
+        @errors << "Schema is not in a valid format"
+        false
+      end
+
+      def ensure_no_unrecognized_keys
+        unrecognized_keys = schema.keys - PERMITTED_KEYS
+        return true if unrecognized_keys.empty?
+
+        @errors << "Unrecognized keys: #{unrecognized_keys.join(', ')}"
+        false
+      end
+
+      def ensure_max_item_levels_is_valid
+        return true unless schema.key?("max_item_levels")
+        return true if [1, 2, 3].include?(schema["max_item_levels"].to_i)
+
+        @errors << "\"max_item_levels\" must be a number between 1 and 3"
+        false
+      end
+
+      def ensure_attributes_are_valid
+        return true unless schema["attributes"]
+
+        schema["attributes"].each do |attribute_schema|
+          attr_validator = Validator::SchemaAttribute.new(
+            attribute: attribute_schema,
+            custom_types: @custom_types,
+            additional_reserved_names: ADDITIONAL_RESERVED_NAMES
+          )
+          next if attr_validator.validate
+
+          @errors << "Attribute \"#{attribute_schema['name']}\" is invalid "\
+                     "- #{attr_validator.errors.join(', ')}"
+        end
+      end
+
+      def attributes_array_of_hashes?(schema)
+        schema["attributes"].is_a?(Array) &&
+          schema["attributes"].all? { |attr| attr.is_a?(Hash) }
+      end
+    end
+  end
+end
---
+++ spec/lib/canvas/validators/menu_schema_spec.rb
@@ -0,0 +1,215 @@
+describe Canvas::Validator::MenuSchema do
+  describe "#validate" do
+    subject(:validator) {
+      Canvas::Validator::MenuSchema.new(schema: schema)
+    }
+
+    describe "#validate" do
+      context "when schema is valid" do
+        let(:schema) {
+          {
+            "max_item_levels" => 2
+          }
+        }
+
+        it "returns true" do
+          expect(validator.validate).to eq(true)
+        end
+      end
+
+      context "when schema is empty" do
+        let(:schema) {
+          {}
+        }
+
+        it "returns true" do
+          expect(validator.validate).to eq(true)
+        end
+      end
+
+      context "when schema is not a hash" do
+        let(:schema) { nil }
+
+        it "returns false with errors" do
+          expect(validator.validate).to eq(false)
+          expect(validator.errors).to include("Schema is not in a valid format")
+        end
+      end
+
+      context "when schema has unrecognized keys" do
+        let(:schema) {
+          {
+            "max_item_levels" => 2,
+            "unknown" => true,
+            "other" => []
+          }
+        }
+
+        it "returns false with errors" do
+          expect(validator.validate).to eq(false)
+          expect(validator.errors).to include("Unrecognized keys: unknown, other")
+        end
+      end
+
+      context "when max_item_levels is nil" do
+        let(:schema) {
+          {
+            "max_item_levels" => nil
+          }
+        }
+
+        it "returns false with errors" do
+          expect(validator.validate).to eq(false)
+          expect(validator.errors).to include("\"max_item_levels\" must be a number between 1 and 3")
+        end
+      end
+
+      context "when max_item_levels is a number as a string" do
+        let(:schema) {
+          {
+            "max_item_levels" => "1"
+          }
+        }
+
+        it "returns true" do
+          expect(validator.validate).to eq(true)
+        end
+      end
+
+      context "when max_item_levels is not a number" do
+        let(:schema) {
+          {
+            "max_item_levels" => "hello"
+          }
+        }
+
+        it "returns false with errors" do
+          expect(validator.validate).to eq(false)
+          expect(validator.errors).to include("\"max_item_levels\" must be a number between 1 and 3")
+        end
+      end
+
+      context "when max_item_levels is greater than 3" do
+        let(:schema) {
+          {
+            "max_item_levels" => 4
+          }
+        }
+
+        it "returns false with errors" do
+          expect(validator.validate).to eq(false)
+          expect(validator.errors).to include("\"max_item_levels\" must be a number between 1 and 3")
+        end
+      end
+
+      context "when the `attributes` key is not an hash" do
+        let(:schema) {
+          {
+            "attributes" => "something wrong"
+          }
+        }
+
+        it "returns false with errors" do
+          expect(validator.validate).to eq(false)
+          expect(validator.errors).to include("Schema is not in a valid format")
+        end
+      end
+
+      context "when the `attributes` is valid" do
+        let(:schema) {
+          {
+            "attributes" => [
+              {
+                "name" => "title",
+                "type" => "string"
+              },
+              {
+                "name" => "images",
+                "type" => "image",
+                "label" => "Cool image"
+              }
+            ]
+          }
+        }
+
+        it "returns true" do
+          expect(validator.validate).to eq(true)
+        end
+      end
+
+      context "when the `attributes` has invalid attributes" do
+        let(:schema) {
+          {
+            "attributes" => [
+              {
+                "name" => "images",
+                "type" => "invalid"
+              }
+            ]
+          }
+        }
+
+        it "returns false with errors" do
+          expect(validator.validate).to eq(false)
+          expect(validator.errors).to include("Attribute \"images\" is invalid - \"type\" must be one of: image, product, post, page, link, text, string, boolean, number, color, select, range, radio, variant")
+        end
+      end
+    end
+
+    context "when the `attributes` has reserved key names" do
+      let(:schema) {
+        {
+          "attributes" => [
+            {
+              "name" => "items",
+              "type" => "String",
+              "label" => "I'm invalid"
+            }
+          ]
+        }
+      }
+
+      it "returns false with errors" do
+        expect(validator.validate).to eq(false)
+        expect(validator.errors).to include("Attribute \"items\" is invalid - \"name\" can't be one of these reserved words: page, company, cart, flash, block, items, type")
+      end
+    end
+
+    context "when schema is using a custom type" do
+      let(:card_type) {
+        {
+          "key" => "Card",
+          "name" => "Card",
+          "attributes" => [
+            { "name" => "title", "type" => "string", "default" => "Hello world" },
+            { "name" => "body", "type" => "text" }
+          ]
+        }
+      }
+
+      subject(:validator) {
+        Canvas::Validator::MenuSchema.new(schema: schema, custom_types: [card_type])
+      }
+
+      let(:schema) {
+        {
+          "attributes" => [
+            {
+              "name" => "title",
+              "type" => "string"
+            },
+            {
+              "name" => "cards",
+              "type" => "Card",
+              "array" => true
+            }
+          ]
+        }
+      }
+
+      it "returns true" do
+        expect(validator.validate).to eq(true)
+      end
+    end
+  end
+end
